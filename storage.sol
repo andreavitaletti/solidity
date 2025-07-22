d105cd5a6b99e614953292ff7427004e6765dd95
Andrea Vitaletti <andrea.vitaletti@gmail.com>
2025-07-22 20:41:56 +0200

    use of the struct to store a name and an amount

--- storage.sol
+++ storage.sol
@@ -2,31 +2,23 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-/**
- * @title Storage
- * @dev Store & retrieve value in a variable
- * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
- */
-
 contract Storage {
-    uint256 number;
 
-    /**
-     * @dev Store value in variable
-     * @param num value to store
-     */
-    function store(uint256 num) public payable {
-        // if require condition is unmet the transaction is automatically reverted
-        // an error message is generated
-        require(msg.value >= 5 wei);
-        number = num;
+    struct Person {
+       string name;
+       uint256 amount;
     }
 
-    /**
-     * @dev Return value
-     * @return value of 'number'
-     */
-    function retrieve() public view returns (uint256) {
-        return number;
+    Person public person;
+    
+    function store(string memory _name, uint256 _amount) public payable {
+        // if require condition is unmet the transaction is automatically reverted
+        require(msg.value >= 5 wei);
+        person.name=_name;
+        person.amount=_amount;
+    }
+
+    function retrieve() public view returns (Person memory) {
+        return person;
     }
 }
